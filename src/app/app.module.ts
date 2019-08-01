16cc64f6f09279043c02755d9eebc8329e35ac9a
Francisco Sevilla <fsevilla@gmail.com>
2019-07-31 19:26:10 -0500

    Reactive form validation in signup form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,6 +8,8 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { HomeComponent } from './components/home/home.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { InputStatusDirective } from './globals/directives/input-status.directive';
 
 @NgModule({
   declarations: [
@@ -15,11 +17,14 @@ import { HomeComponent } from './components/home/home.component';
     LoginComponent,
     NotFoundComponent,
     HomeComponent,
+    SignupComponent,
+    InputStatusDirective,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [],
---
+++ src/app/globals/directives/input-status.directive.spec.ts
@@ -0,0 +1,8 @@
+import { InputStatusDirective } from './input-status.directive';
+
+describe('InputStatusDirective', () => {
+  it('should create an instance', () => {
+    const directive = new InputStatusDirective();
+    expect(directive).toBeTruthy();
+  });
+});
